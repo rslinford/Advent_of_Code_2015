677f244d821fac51c3be7feb8df06f3fc24b7e36
Scott <scott@linfords.com>
2022-08-20 04:28:58 -0700

    Day 01 - passes one and two

--- Day_01.py
+++ Day_01.py
@@ -16,13 +16,29 @@ def find_depth(data):
                 depth -= 1
     return depth
 
+def find_basement(data):
+    depth = 0
+    for i, c in enumerate(data):
+        match c:
+            case '(':
+                depth += 1
+            case ')':
+                depth -= 1
+        if depth < 0:
+            return i + 1
+    return depth
+
 
 def part_one(filename):
     data = read_puzzle_input(filename)
     print('Depth', find_depth(data))
 
+def part_two(filename):
+    data = read_puzzle_input(filename)
+    print('Enters basement at position', find_basement(data))
 
-part_one('Day_01_input.txt')
+
+part_two('Day_01_input.txt')
 
 class Test(unittest.TestCase):
     def test_find_depth(self):
@@ -32,3 +48,7 @@ class Test(unittest.TestCase):
         self.assertEqual(3, find_depth('(()(()('))
         self.assertEqual(-1, find_depth('())'))
         self.assertEqual(-3, find_depth(')())())'))
+
+    def test_find_basement(self):
+        self.assertEqual(1, find_basement(')'))
+        self.assertEqual(5, find_basement('()()))'))
