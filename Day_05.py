9b8008f0f82bf51612402d2b14e41eef498fa46b
Scott <scott@linfords.com>
2022-08-21 08:01:30 -0700

    Day 05 - part one in progress

--- Day_05.py
+++ Day_05.py
@@ -4,8 +4,33 @@ def read_puzzle_data(filename):
     return data
 
 
+def has_at_least_three_vowels(candidate):
+    pass
+
+
+def has_double_letter(candidate):
+    pass
+
+
+def is_nice(candidate):
+    disallowed = ['ab', 'cd', 'pq', 'xy']
+    for d in disallowed:
+        if candidate.find(d) != -1:
+            return False
+        if not has_at_least_three_vowels(candidate):
+            return False
+        if not has_double_letter(candidate):
+            return False
+    return True
+
+
 def part_one(filename):
     data = read_puzzle_data(filename)
+    tally = 0
+    for s in data:
+        if is_nice(s):
+            tally += 1
+    return tally
 
 
-part_one('Day_05_input.txt')
+print(part_one('Day_05_input.txt'))
