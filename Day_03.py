1a1a8f3426479a1f3f51f08df8c0a8e1b0f7de48
Scott <scott@linfords.com>
2022-08-20 23:13:19 -0700

    Day 03 - part one passes

--- Day_03.py
+++ Day_03.py
@@ -1,12 +1,38 @@
+import unittest
+
+
 def read_puzzle_data(filename):
     with open(filename, 'r') as f:
         directions = f.read()
     return directions
 
+def follow_directions(directions):
+    x, y = 0, 0
+    history = set()
+    history.add((0, 0))
+    for direction in directions:
+        match direction:
+            case '^':
+                y += 1
+            case '>':
+                x += 1
+            case 'v':
+                y -= 1
+            case '<':
+                x -= 1
+        point = (x, y)
+        history.add(point)
+    return len(history)
 
 def part_one(filename):
-    data = read_puzzle_data(filename)
-    print(data)
+    directions = read_puzzle_data(filename)
+    return follow_directions(directions)
 
 
-part_one('Day_03_input.txt')
+print('Houses that receive at least one present', part_one('Day_03_input.txt'))
+
+class Test(unittest.TestCase):
+    def test_follow_directions(self):
+        self.assertEqual(4, follow_directions('^>v<'))
+        self.assertEqual(2, follow_directions('<'))
+        self.assertEqual(2, follow_directions('^v^v^v^v^v'))
