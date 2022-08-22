f094fd78e72bb9c87755a4da559071fe432927ad
Scott <scott@linfords.com>
2022-08-21 23:11:43 -0700

    Day 06 - part one passes, part two in progress

--- Day_06.py
+++ Day_06.py
@@ -17,7 +17,7 @@ class LightGrid:
         self.grid[:] = '.'
     def __repr__(self):
         rval = []
-        for y in range(self.grid.shape[0]-1, 0, -1):
+        for y in range(self.grid.shape[0]-1, -1, -1):
             for x in range(self.grid.shape[1]):
                 match self.grid[y][x]:
                     case b'.':
@@ -26,7 +26,8 @@ class LightGrid:
                         rval.append('#')
                     case _:
                         raise ValueError
-            rval.append('\n')
+            if y > 0:
+                rval.append('\n')
         return ''.join(rval)
 
     def turn_on(self, x_index, y_index):
@@ -43,6 +44,48 @@ class LightGrid:
                 self.grid[y_index][x_index] = '.'
             case _:
                 raise ValueError
+
+    def tally_lit(self):
+        tally = 0
+        for y in range(self.grid.shape[0]):
+            for x in range(self.grid.shape[1]):
+                if self.grid[y][x] == b'#':
+                    tally += 1
+        return tally
+
+class LightGridMarkTwo:
+    grid: np.array
+
+    def __init__(self, width, height):
+        self.grid = np.zeros((height, width), dtype=np.uint)
+    def __repr__(self):
+        rval = []
+        for y in range(self.grid.shape[0]-1, -1, -1):
+            for x in range(self.grid.shape[1]):
+                if x > 0:
+                    rval.append(', ')
+                rval.append(str(self.grid[y][x]))
+            if y > 0:
+                rval.append('\n')
+        return ''.join(rval)
+
+    def turn_on(self, x_index, y_index):
+        self.grid[y_index][x_index] += 1
+
+    def turn_off(self, x_index, y_index):
+        if self.grid[y_index][x_index] > 0:
+            self.grid[y_index][x_index] -= 1
+
+    def toggle(self, x_index, y_index):
+        self.grid[y_index][x_index] += 2
+
+    def tally_lit(self):
+        tally = 0
+        for y in range(self.grid.shape[0]):
+            for x in range(self.grid.shape[1]):
+                if self.grid[y][x] == b'#':
+                    tally += 1
+        return tally
 
 
 def parse_line(command_text):
@@ -72,24 +115,178 @@ def process_command(grid: LightGrid, command, x, y):
                 case 'toggle':
                     grid.toggle(x_index, y_index)
 
+def process_command_mark_two(grid: LightGridMarkTwo, command, x, y):
+    if x[0] > x[1]:
+        x[0], x[1] = x[1], x[0]
+    if y[0] > y[1]:
+        y[0], y[1] = y[1], y[0]
+    for y_index in range(y[0], y[1] + 1):
+        for x_index in range(x[0], x[1] + 1):
+            match command:
+                case 'turn on':
+                    grid.turn_on(x_index, y_index)
+                case 'turn off':
+                    grid.turn_off(x_index, y_index)
+                case 'toggle':
+                    grid.toggle(x_index, y_index)
+
 def light_show(command_input):
     grid = LightGrid(1000, 1000)
     for command in command_input:
         parsed_command = parse_line(command)
         process_command(grid, *parsed_command)
-        print(grid)
+    return grid.tally_lit()
+
+def light_show_mark_two(command_input):
+    grid = LightGridMarkTwo(1000, 1000)
+    for command in command_input:
+        parsed_command = parse_line(command)
+        process_command_mark_two(grid, *parsed_command)
+    return grid.tally_lit()
 
 def part_one(filename):
     command_input = read_puzzle_input(filename)
-    light_show(command_input)
+    return light_show(command_input)
 
-part_one('Day_06_input.txt')
+def part_two(filename):
+    command_input = read_puzzle_input(filename)
+    return light_show_mark_two(command_input)
+
+# print('Lights lit:', part_two('Day_06_input.txt'))
+
 
 class Test(unittest.TestCase):
     def test_parse_line(self):
         self.assertEqual(('turn on', [887, 959], [9, 629]), parse_line('turn on 887,9 through 959,629'))
         self.assertEqual(('toggle', [720, 897], [196, 994]), parse_line('toggle 720,196 through 897,994'))
 
+class TestLightGridMarkTwo(unittest.TestCase):
+    def test_light_grid_mark_two(self):
+        grid = LightGridMarkTwo(4, 4)
+        self.assertEqual("0, 0, 0, 0\n"
+                         "0, 0, 0, 0\n"
+                         "0, 0, 0, 0\n"
+                         "0, 0, 0, 0", str(grid))
+        process_command_mark_two(grid, *parse_line('turn on 0,0 through 3,3'))
+        self.assertEqual("1, 1, 1, 1\n"
+                         "1, 1, 1, 1\n"
+                         "1, 1, 1, 1\n"
+                         "1, 1, 1, 1", str(grid))
+        process_command_mark_two(grid, *parse_line('turn off 0,0 through 3,3'))
+        self.assertEqual("0, 0, 0, 0\n"
+                         "0, 0, 0, 0\n"
+                         "0, 0, 0, 0\n"
+                         "0, 0, 0, 0", str(grid))
+        process_command_mark_two(grid, *parse_line('toggle 0,0 through 3,3'))
+        self.assertEqual("2, 2, 2, 2\n"
+                         "2, 2, 2, 2\n"
+                         "2, 2, 2, 2\n"
+                         "2, 2, 2, 2", str(grid))
+        process_command_mark_two(grid, *parse_line('turn off 0,0 through 3,3'))
+        process_command_mark_two(grid, *parse_line('turn off 0,0 through 3,3'))
+        process_command_mark_two(grid, *parse_line('turn off 0,0 through 3,3'))
+        self.assertEqual("0, 0, 0, 0\n"
+                         "0, 0, 0, 0\n"
+                         "0, 0, 0, 0\n"
+                         "0, 0, 0, 0", str(grid))
+
+
 class TestLightGrid(unittest.TestCase):
-    command_input = read_puzzle_input('Day_06_short_input.txt')
-    light_show(command_input)
+    def test_light_grid(self):
+        grid = LightGrid(10, 10)
+        self.assertEqual("..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........", str(grid))
+        self.assertEqual(0, grid.tally_lit())
+        process_command(grid, *parse_line('turn on 0,0 through 9,9'))
+        self.assertEqual("##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########", str(grid))
+        self.assertEqual(100, grid.tally_lit())
+        process_command(grid, *parse_line('turn off 0,0 through 9,9'))
+        self.assertEqual("..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........", str(grid))
+        self.assertEqual(0, grid.tally_lit())
+        process_command(grid, *parse_line('turn on 0,0 through 0,0'))
+        self.assertEqual("..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "#.........", str(grid))
+        self.assertEqual(1, grid.tally_lit())
+        process_command(grid, *parse_line('turn on 9,0 through 9,0'))
+        self.assertEqual("..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "#........#", str(grid))
+        self.assertEqual(2, grid.tally_lit())
+        process_command(grid, *parse_line('turn on 0,9 through 0,9'))
+        self.assertEqual("#.........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "#........#", str(grid))
+        self.assertEqual(3, grid.tally_lit())
+        process_command(grid, *parse_line('turn on 9,9 through 9,9'))
+        self.assertEqual("#........#\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "..........\n"
+                         "#........#", str(grid))
+        self.assertEqual(4, grid.tally_lit())
+        process_command(grid, *parse_line('toggle 0,0 through 9,9'))
+        self.assertEqual(".########.\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         "##########\n"
+                         ".########.", str(grid))
+        self.assertEqual(96, grid.tally_lit())
+
