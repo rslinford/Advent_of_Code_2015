ee75ff84832de42f1ea5d5ba4a61ab8f9b207f78
Scott <scott@linfords.com>
2022-08-25 19:38:57 -0700

    Day 09 - edges parsed into Edges

--- Day_09.py
+++ Day_09.py
@@ -1,13 +1,32 @@
+import re
+
+class Edge:
+    def __init__(self, from_city, to_city, distance):
+        self.from_city = from_city
+        self.to_city = to_city
+        self.distance = distance
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} from_city({self.from_city}) to_city({self.to_city}) distance({self.distance})'
+
 def read_puzzle_input(filename):
     with open(filename, 'r') as f:
         data = f.read().strip().split('\n')
     return data
 
 
+def parse_line(line):
+    result = re.search(r'(\w+) to (\w+) = (\d+)', line)
+    return result.group(1), result.group(2), int(result.group(3))
+
+
+def build_graph(data):
+    for line in data:
+        edge = Edge(*parse_line(line))
+        print(edge)
+
 def part_one(filename):
     data = read_puzzle_input(filename)
-    for x in data:
-        print(x)
+    build_graph(data)
 
-
-part_one('Day_09_input.txt')
+part_one('Day_09_short_input.txt')
