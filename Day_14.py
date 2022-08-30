ab15dbc4d35b6c21c89dc0bdf594a13a9ae60c60
Scott <scott@linfords.com>
2022-08-30 01:43:09 -0700

    Day 14 - Reindeer deserialization complete, working on part one.

--- Day_14.py
+++ Day_14.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 
 
@@ -7,17 +8,43 @@ def read_puzzle_input(filename):
     return data
 
 
+class Reindeer:
+    def __init__(self, name, speed, speed_duration, rest_duration):
+        self.name = name
+        self.speed = speed
+        self.speed_duration = speed_duration
+        self.rest_duration = rest_duration
+
+    def __repr__(self):
+        return f'name({self.name}) speed({self.speed}) speed_duration({self.speed_duration}) rest_duration({self.rest_duration})'
+
+
+def deserialize_reindeer(data):
+    reindeer = []
+    for line in data:
+        result = re.search(r'^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds\.$',
+                           line)
+        name = result.group(1)
+        speed = int(result.group(2))
+        speed_duration = int(result.group(3))
+        rest_duration = int(result.group(4))
+        reindeer.append(Reindeer(name, speed, speed_duration, rest_duration))
+    return reindeer
+
 
 def part_one(filename):
     data = read_puzzle_input(filename)
+    reindeer = deserialize_reindeer(data)
+    for x in reindeer:
+        print(x)
 
 
-short = False
+short = True
 
 if short:
-    print('Answer:', part_one('Day_13_short_input.txt'))
+    print('Answer:', part_one('Day_14_short_input.txt'))
 else:
-    print('Answer:', part_one('Day_13_input_with_me.txt'))
+    print('Answer:', part_one('Day_14_input.txt'))
 
 
 class Test(unittest.TestCase):
