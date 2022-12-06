c222e2bcf6bdadd184601f4fbc46a0e0bbdcf571
Scott <scott@linfords.com>
2022-12-06 10:20:01 -0800

    2015 Day 20 initial commit

---
+++ Day_20.py
@@ -0,0 +1,23 @@
+import unittest
+
+puzzle_input = 29000000
+
+
+def part_one():
+    return -1
+
+
+def part_two():
+    return -1
+
+
+print(f'Answer part one: {part_one()}')
+print(f'Answer part two: {part_two()}')
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        self.assertEqual(-1, part_one())
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two())
